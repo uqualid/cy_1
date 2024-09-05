3804a16b512640fed8ccc069d26443bdac523e36
uqualid <uqualid@tukorea.ac.kr>
2024-09-05 17:55:05 +0900

    0905 - JWT Token api 설계/ logout 해결

---
+++ src/main/java/com/example/board/user/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.board.user.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swaggerApi() {
+        Server server = new Server()
+                .url("/")
+                .description("Default Server url");
+
+        // Security Scheme 설정 추가
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .addServersItem(server)
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
+    }
+}
